a3403daeb66bd766aa2f8076b72271380038f03e
alrigthk <kxndziu@onet.pl>
2021-08-05 12:11:09 +0200

    working version of TEST inputing numbers

--- TEST/TEST.rb
+++ TEST/TEST.rb
@@ -1,2 +1,31 @@
-gets 1,2,88,42,99
-puts 1,2,88
+gets "1"
+gets "2"
+gets "88"
+gets "42"
+gets "99"
+
+sleep(0.7)
+
+puts " "
+puts "1"
+puts "2"
+puts "88"
+puts " "
+puts " "
+
+sleep(1)
+
+require 'colorize'
+
+puts "Type 'done'".light_red
+puts " "
+puts " "
+
+gets "done"
+
+sleep(1)
+
+require 'colorize'
+
+puts " "
+puts "Good job!".green
